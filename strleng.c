6c14ef74ec9c173eaed7bbc44bcf7f67213b2f7d
Himanshu Gupta <himanshugupta543210@gmail.com>
2023-07-16 17:28:51 +0530

    first commit

---
+++ strleng.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    // Write C code here
+    int n=1;
+    int count=0;
+    char x[]="yogendra";
+    for(int i=0; x[i]!=0 ;i++){
+     count++; 
+    }
+    printf("%d", count);
+
+    return 0;
+}
